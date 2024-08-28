6d366fd699c544d163211bdbcffe243aa5b54414
vitolev <vito.levstik@gmail.com>
2024-08-28 13:06:31 +0200

    Staticfiles update

--- skeniraj_in_naroci/settings.py
+++ skeniraj_in_naroci/settings.py
@@ -123,14 +123,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-STATIC_URL = 'static/'
+STATIC_URL = '/static/' #suggested by chat for retrievent static files
 
 STATICFILES_DIRS = [
     BASE_DIR / "users" / "static",
     BASE_DIR / "owners" / "static",
+    os.path.join(BASE_DIR, 'skeniraj-in-naroci/static'),
 ]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
